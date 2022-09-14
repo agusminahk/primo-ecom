dd1870043b59af55da52609701f5dc0266883711
agusminahk <agusminahk99@gmail.com>
2022-09-14 20:14:10 -0300

    test

--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -2,8 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
     reducer: {},
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false,
-        }),
+
 });
