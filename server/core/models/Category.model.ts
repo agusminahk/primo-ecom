8df8ee2a4606b02c1b115502befa1a3557633fe8
agusminahk <agusminahk99@gmail.com>
2022-09-20 13:35:59 -0300

    fix: decorators

--- server/core/models/Category.model.ts
+++ server/core/models/Category.model.ts
@@ -1,7 +1,6 @@
 import { prop, getModelForClass, modelOptions } from "@typegoose/typegoose";
 
 @modelOptions({ schemaOptions: { collection: "category", timestamps: true } })
-
 export class Category {
     @prop({ type: () => String, required: true, unique: true })
     public categoryName!: string;
--- server/core/models/SubCategory.model.ts
+++ server/core/models/SubCategory.model.ts
@@ -1,7 +1,7 @@
 import { prop, getModelForClass, modelOptions, Ref } from "@typegoose/typegoose";
 import { Category } from "./Category.model";
-@modelOptions({ schemaOptions: { collection: "sub_category", timestamps: true } })
 
+@modelOptions({ schemaOptions: { collection: "sub_category", timestamps: true } })
 export class SubCategory {
     @prop({ type: () => String, required: true, unique: true })
     public subCategoryName!: string;
