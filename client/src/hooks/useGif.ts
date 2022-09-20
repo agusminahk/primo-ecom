72b7f9a659100cf603c86762ba34b4cb3aa2705b
Francisco Gramajo <frangrama510@gmail.com>
2022-09-20 15:10:05 -0300

    feat: adding Colors and Gif interfaces

--- client/src/hooks/useGif.ts
+++ client/src/hooks/useGif.ts
@@ -4,24 +4,24 @@ import { gifs as arrayGifs } from '../utils/home';
 import type { Gifs, Title } from '../utils/interfaces';
 
 interface Colors {
-  P?: string
-  R?: string
-  I?: string
-  M?: string
-  O?: string
-  ['']?: string
+  P?: string;
+  R?: string;
+  I?: string;
+  M?: string;
+  O?: string;
+  ['']?: string;
 }
 
 const colors: Colors = {
-  P: '#31b0cc',
-  R: 'violet',
-  I: 'white',
-  M: 'pink',
-  O: '#ff4d00',
+  P: '#124670',
+  R: '#701217',
+  I: '#88418c',
+  M: '#12706b',
+  O: '#c7ece6',
 };
 
 interface Gif {
-  type?: Title
+  type?: Title;
   url?: string;
   isActive?: boolean;
   color?: string;
@@ -31,10 +31,14 @@ const useGif = () => {
   const [gif, setGif] = useState<Gif>({ type: '', url: '', color: '', isActive: false });
 
   useEffect(() => {
-    arrayGifs.map(({ letter, url }: Gifs) => gif.type === letter && setGif({ ...gif, type: letter, url, color: colors[letter] }));
+    arrayGifs.map(
+      ({ letter, url }: Gifs) => gif.type === letter && setGif({ ...gif, type: letter, url, color: colors[letter] }),
+    );
   }, [gif.isActive]);
 
   return { gif, setGif };
 };
 
 export default useGif;
+
+// P: '#31b0cc',
