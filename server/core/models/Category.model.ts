555efaa73d240c389c88c7a462b35dde13981896
agusminahk <agusminahk99@gmail.com>
2022-09-20 13:34:11 -0300

    feat: Category and SubCategory Models

---
+++ server/core/models/Category.model.ts
@@ -0,0 +1,11 @@
+import { prop, getModelForClass, modelOptions } from "@typegoose/typegoose";
+
+@modelOptions({ schemaOptions: { collection: "category", timestamps: true } })
+
+export class Category {
+    @prop({ type: () => String, required: true, unique: true })
+    public categoryName!: string;
+
+}
+
+export default getModelForClass(Category)
---
+++ server/core/models/SubCategory.model.ts
@@ -0,0 +1,14 @@
+import { prop, getModelForClass, modelOptions, Ref } from "@typegoose/typegoose";
+import { Category } from "./Category.model";
+@modelOptions({ schemaOptions: { collection: "category", timestamps: true } })
+
+export class SubCategory {
+    @prop({ type: () => String, required: true, unique: true })
+    public subCategoryName!: string;
+
+    @prop({ type: () => Category, required: true })
+    public category!: Ref<Category>;
+
+}
+
+export default getModelForClass(SubCategory)
