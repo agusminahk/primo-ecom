ab4eb5277b8ac7a660cd0822f15d383fe46009de
Francisco Gramajo <frangrama510@gmail.com>
2022-09-29 16:39:44 -0300

    refactor: reordering styles

--- client/src/common/ProductCard.tsx
+++ client/src/common/ProductCard.tsx
@@ -1,21 +1,11 @@
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardActionArea,
-  CardMedia,
-  Divider,
-  IconButton,
-  Typography,
-  Box,
-  keyframes,
-} from '@mui/material';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Card, CardActionArea, CardContent, CardMedia, IconButton, Typography, Box, keyframes } from '@mui/material';
+
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import PlusOneIcon from '@mui/icons-material/PlusOne';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import React, { useState } from 'react';
+
+import React, { FC, useState } from 'react';
 
 export interface TypeRopa {
   indice: string;
@@ -29,11 +19,35 @@ type CardProps = {
   product: TypeRopa;
 };
 
-const ProductCard: React.FC<CardProps> = ({ product }) => {
-  const [hover, setHover] = useState(false);
-  const [hoverCartButton, setHoverCartButton] = useState(false);
+const ProductCard: FC<CardProps> = ({ product }) => {
+  const [hover, setHover] = useState<boolean>(false);
+  const [hoverCartButton, setHoverCartButton] = useState<boolean>(false);
 
-  const likeAnimation = keyframes`
+  const priceAnimation = keyframes`
+	0% {
+		opacity: 1;
+		transform: translateY(0);
+	}
+
+	100% {
+		opacity: 1;
+		transform: translateY(-18px);
+	}
+  `;
+
+  const titleAnimation = keyframes`
+	0% {
+		opacity: 1;
+		transform: translateY(140px);
+	}
+
+	100% {
+		opacity: 1;
+		transform: translateY(0);
+	}
+  `;
+
+  const btnAnimation = keyframes`
   0% {
     transform: scale(0.75)
   }
@@ -46,117 +60,115 @@ const ProductCard: React.FC<CardProps> = ({ product }) => {
     cardStyle: {
       boxShadow: '3',
       width: '12rem',
+      position: 'relative',
+      borderRadius: '3%',
+    },
+    boxTypoStyle: {
+      top: '170.5px',
+      backgroundColor: 'primaryTransparent.light',
+      backdropFilter: 'blur(50px)',
+      display: 'flex',
+      width: '100%',
+      justifyContent: 'center',
+      position: 'absolute',
+      borderTopLeftRadius: '10px 10px',
+      borderTopRightRadius: '10px 10px',
+      animation: `${titleAnimation} 0.15s both`,
+    },
+    typoStyle: {
+      px: '7px',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+    },
+    dividerStyle: {
+      my: 1,
+    },
+    cardMediaStyle: {
+      width: '12rem',
+      height: '12rem',
     },
     cardActionStyle: {
       display: 'flex',
       justifyContent: 'center',
       flexDirection: 'column',
-    },
-
-    contentStyle: {
-      py: 2,
-    },
-    mediaStyle: {
-      justifyContent: 'center',
+      position: 'absolute',
+      top: '10px',
+      left: '155px',
     },
     iconStyle: {
-      color: 'warning.main',
+      animation: `${btnAnimation} 0.25s both`,
+      backgroundColor: 'neutral.main',
+      color: 'primary.main',
+      my: '2px',
+      '&:hover': { color: 'highlight.main', backgroundColor: 'neutral.main' },
     },
-    typoStyle: {
-      overflow: 'hidden',
-      whiteSpace: 'nowrap',
-      textOverflow: 'ellipsis',
-      '&:hover': { overflow: 'hidden', textOverflow: 'ellipsis', whitSpace: 'normal' },
-    },
-    dividerStyle: {
-      py: 0.5,
-      m: 0.5,
-    },
-    boxStyle: {
+    priceBoxStyle: {
+      backgroundColor: 'neutral.main',
+      borderRadius: '25px',
       display: 'flex',
+      alignItems: 'center',
+      position: 'absolute',
+      width: 'auto',
+      top: '160px',
+      animation: hover && `${priceAnimation} 0.3s both`,
+      left: '5px',
+    },
+    moneyIconStyle: {
+      my: '2px',
+      color: 'highlight.main',
+    },
+    priceTypoStyle: {
+      color: 'primary.main',
+      mr: '8px',
+      userSelect: 'none',
     },
   };
 
   return (
     <Card
-      sx={{ ...style.cardStyle, position: 'relative' }}
+      sx={style.cardStyle}
       onMouseOver={e => {
         setHover(true);
       }}
       onMouseOut={e => {
         setHover(false);
       }}>
-      <CardContent sx={style.contentStyle}>
-        <CardActionArea>
-          <Typography variant={'h2'} sx={style.typoStyle}>
-            {product.name}
-          </Typography>
-        </CardActionArea>
-        <Divider sx={style.dividerStyle}></Divider>
-        <Typography component="div" variant={'h5'} sx={style.typoStyle}>
-          {product.description}
-        </Typography>
-      </CardContent>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          sx={{
-            width: '12rem',
-            height: '12rem',
-            borderRadius: '3%',
-          }}
-          image={product.image}
-          alt="clothe"
-        />
+        <CardMedia component="img" sx={style.cardMediaStyle} image={product.image} alt={product.name} />
       </CardActionArea>
-      {hover && (
-        <Box sx={{ ...style.cardActionStyle, position: 'absolute', top: '100px', left: '155px' }}>
-          <IconButton
-            size="small"
-            sx={{
-              animation: `${likeAnimation} 0.25s both`,
-              backgroundColor: 'neutral.main',
-              color: 'primary.main',
-              my: '2px',
-              '&:hover': { color: 'highlight.main', backgroundColor: 'neutral.main' },
-            }}>
-            <FavoriteBorderIcon fontSize="small" />
-          </IconButton>
-          <IconButton
-            size="small"
-            sx={{
-              animation: `${likeAnimation} 0.25s both`,
-              backgroundColor: 'neutral.main',
-              color: 'primary.main',
-              my: '2px',
-              '&:hover': { color: 'highlight.main', backgroundColor: 'neutral.main' },
-            }}
-            onMouseEnter={e => {
-              setHoverCartButton(true);
-            }}
-            onMouseLeave={e => {
-              setHoverCartButton(false);
-            }}>
-            {!hoverCartButton ? <LocalMallIcon fontSize="small" /> : <PlusOneIcon fontSize="small" />}
-          </IconButton>
-        </Box>
-      )}
-      <Box
-        sx={{
-          backgroundColor: 'neutral.main',
-          borderRadius: '25px',
-          display: 'flex',
-          alignItems: 'center',
-          position: 'absolute',
-          width: 'auto',
-          top: '250px',
-          left: '5px',
-        }}>
-        <AttachMoneyIcon fontSize={'inherit'} sx={{ my: '2px', color: 'highlight.main' }} />
-        <Typography variant="h6" sx={{ color: 'primary.main', mr: '8px', userSelect: 'none' }}>
+      <Box sx={style.priceBoxStyle}>
+        <AttachMoneyIcon fontSize={'inherit'} sx={style.moneyIconStyle} />
+        <Typography variant="h6" sx={style.priceTypoStyle}>
           {product.price}
         </Typography>
       </Box>
+      {hover && (
+        <>
+          <Box sx={style.cardActionStyle}>
+            <IconButton size="small" sx={style.iconStyle}>
+              <FavoriteBorderIcon fontSize="small" />
+            </IconButton>
+            <IconButton
+              size="small"
+              sx={style.iconStyle}
+              onMouseEnter={e => {
+                setHoverCartButton(true);
+              }}
+              onMouseLeave={e => {
+                setHoverCartButton(false);
+              }}>
+              {!hoverCartButton ? <LocalMallIcon fontSize="small" /> : <PlusOneIcon fontSize="small" />}
+            </IconButton>
+          </Box>
+
+          <Box sx={style.boxTypoStyle}>
+            <Typography variant="subtitle2" sx={style.typoStyle}>
+              {product.name}
+            </Typography>
+          </Box>
+        </>
+      )}
     </Card>
   );
 };
