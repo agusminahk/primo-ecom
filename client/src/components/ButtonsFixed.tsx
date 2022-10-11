575d37a6e39cad2e8b653f250a2d9a979c7bc77f
Fran <frangrama510@gmail.com>
2022-10-11 16:36:58 -0300

    refactor: new styles

--- client/src/components/ButtonsFixed.tsx
+++ client/src/components/ButtonsFixed.tsx
@@ -4,36 +4,48 @@ import UpIcon from '@mui/icons-material/KeyboardArrowUp';
 import React, { FC, useState } from 'react';
 import CartDrawer from './CartDrawer';
 
-const ButtonsFixed: FC = () => {
+const ButtonsFixed: FC<{ inView: boolean }> = ({ inView }) => {
   const [open, setOpen] = useState<boolean>(false);
 
   const fabAnimation = keyframes`
   0% {
-    transform: rotate(0) scale(0.5)
+    transform: rotate(0) scale(0.6)
   }
   100% {
     transform: rotate(360deg) scale(1)
   }
   `;
 
+  const fabAnimationReverse = keyframes`
+  0% {
+    transform: rotate(0) scale(1);
+    opacity: 90%
+  }
+  100% {
+    transform: rotate(0) scale(0.7); 
+    opacity: 0%
+  }
+  `;
+
   const styles = {
     firstBoxStyle: {
       position: 'fixed',
-      mt: '85vh',
-      ml: '90%',
+      left: '95%',
+      top: '86%',
       display: 'flex',
       alignContent: 'center',
       flexDirection: 'column',
+      zIndex: 99999,
     },
     cartFabStyle: {
       backgroundColor: 'highlight.main',
       m: '4%',
-      animation: `${fabAnimation} 0.7s both`,
+      animation: inView ? `${fabAnimationReverse} 0.4s both` : `${fabAnimation} 0.3s both`,
     },
     scrollFabStyle: {
       backgroundColor: 'highlight.main',
       m: '4%',
-      animation: `${fabAnimation} 0.7s both`,
+      animation: inView ? `${fabAnimationReverse} 0.4s both` : `${fabAnimation} 0.3s both`,
     },
   };
 
