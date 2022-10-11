802a9ff44523916e06fbc90e0a06ce03beec9342
Fran <frangrama510@gmail.com>
2022-10-11 16:41:39 -0300

    feat: adding custom dots as cards

--- client/src/components/CustomCarousel.tsx
+++ client/src/components/CustomCarousel.tsx
@@ -1,7 +1,7 @@
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import { IconButton } from '@mui/material';
+import { IconButton, Button, Card, Box, CardMedia, keyframes } from '@mui/material';
 
 import React, { FC } from 'react';
 
@@ -13,15 +13,130 @@ const CustomDot: FC = ({ onClick, active }: any) => {
   );
 };
 
+const CustomDotMobile: FC = ({ onClick, active }: any) => {
+  return (
+    <Box
+      component="div"
+      onClick={() => onClick()}
+      sx={{
+        width: '10%',
+        height: '2px',
+        bgcolor: active ? 'highlight.main' : 'primaryTransparent.main',
+        backdropFilter: active ? 'blur(50px)' : 'invert(100%) blur(30px) ',
+        m: '1%',
+        borderRadius: '25px',
+      }}></Box>
+  );
+};
+
+const btnAnimation = keyframes`
+0% {
+  transform: scale(1); opacity: 85%
+}
+100% {
+  transform: scale(1.1); opacity: 100%
+}
+`;
+
+const btnAnimationBackward = keyframes`
+0% {
+  transform: scale(1.1); opacity: 100%
+}
+100% {
+  transform: scale(1); opacity: 85%
+}
+`;
+
+const btnOpacity = keyframes`
+0% {
+  opacity: 85%
+}
+100% {
+  opacity: 100%
+}
+`;
+
+const CardAsDots = ({ index, onClick, active, images }: any) => {
+  return (
+    <Box
+      component="div"
+      key={index}
+      sx={{
+        animation: active ? `${btnAnimation} 0.25s both` : `${btnAnimationBackward} 0.25s both`,
+        m: '2%',
+        position: 'relative',
+        width: '100%',
+      }}>
+      <Card
+        onClick={() => onClick()}
+        sx={{
+          borderRadius: '0.5px',
+          height: '80%',
+          width: '100%',
+        }}>
+        <CardMedia component="img" image={`${images[index]}`} sx={{ cursor: 'pointer' }} />
+      </Card>
+    </Box>
+  );
+};
+
+const CardAsDots2 = ({ index, onClick, active, images }: any) => {
+  return (
+    <Box
+      component="div"
+      key={index}
+      sx={{
+        animation: active ? `${btnAnimation} 0.25s both` : `${btnAnimationBackward} 0.25s both`,
+      }}>
+      <Card
+        onClick={() => onClick()}
+        sx={{
+          borderRadius: '3px',
+          height: { xl: '5rem', lg: '5rem', md: '4rem', sm: '3rem', xs: '3rem' },
+          width: { xl: '5rem', lg: '5rem', md: '4rem', sm: '3rem', xs: '3rem' },
+        }}>
+        <CardMedia component="img" image={`${images[index]}`} sx={{ cursor: 'pointer' }} />
+      </Card>
+    </Box>
+  );
+};
+
 const LeftArrow: FC = ({ onClick }: any) => {
   return (
-    <IconButton size="medium" sx={{ mt: '10vh' }} className="custom-left-arrow" onClick={() => onClick()}>
+    <IconButton
+      size="medium"
+      sx={{ position: 'absolute', mt: '-20%', ml: '2%' }}
+      className="custom-left-arrow"
+      onClick={() => onClick()}>
       <ArrowCircleLeftIcon fontSize="small" sx={{ color: 'primary.main' }} />
     </IconButton>
   );
 };
 const RightArrow: FC = ({ onClick }: any) => {
-  return <i className="custom-right-arrow" onClick={() => onClick()} />;
+  return (
+    <IconButton
+      size="medium"
+      sx={{ position: 'absolute', mt: '-20%', ml: '92%' }}
+      className="custom-right-arrow"
+      onClick={() => onClick()}>
+      <ArrowCircleRightIcon fontSize="small" sx={{ color: 'primary.main' }} />
+    </IconButton>
+  );
+};
+
+const LeftArrowProduct: FC = ({ onClick }: any) => {
+  return (
+    <IconButton size="medium" sx={{ position: 'absolute' }} className="custom-left-arrow" onClick={() => onClick()}>
+      <ArrowCircleLeftIcon fontSize="small" sx={{ color: 'primary.main' }} />
+    </IconButton>
+  );
+};
+const RightArrowProduct: FC = ({ onClick }: any) => {
+  return (
+    <IconButton size="medium" sx={{ position: 'absolute' }} className="custom-right-arrow" onClick={() => onClick()}>
+      <ArrowCircleRightIcon fontSize="small" sx={{ color: 'primary.main', ml: '100%' }} />
+    </IconButton>
+  );
 };
 
 const CustomButtonGroupAsArrows: FC = ({ onClick, next, previous }: any) => {
@@ -30,7 +145,7 @@ const CustomButtonGroupAsArrows: FC = ({ onClick, next, previous }: any) => {
       style={{
         display: 'flex',
         justifyContent: 'space-around',
-        marginTop: '-4%',
+        marginTop: '-5.5%',
         marginBottom: '4%',
       }}>
       <IconButton size="medium" className="custom-left-arrow" onClick={previous}>
@@ -43,4 +158,14 @@ const CustomButtonGroupAsArrows: FC = ({ onClick, next, previous }: any) => {
   );
 };
 
-export { CustomDot, LeftArrow, RightArrow, CustomButtonGroupAsArrows };
+export {
+  CustomDot,
+  LeftArrow,
+  RightArrow,
+  LeftArrowProduct,
+  RightArrowProduct,
+  CustomButtonGroupAsArrows,
+  CardAsDots,
+  CustomDotMobile,
+  CardAsDots2,
+};
