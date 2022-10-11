c8c542a1e407d292740c952bd38fbd9595221d8e
Fran <frangrama510@gmail.com>
2022-10-11 16:45:59 -0300

    feat: sample of a grid.

--- client/src/pages/samples/index.tsx
+++ client/src/pages/samples/index.tsx
@@ -1,40 +1,14 @@
 import { Box } from '@mui/system';
 import React from 'react';
 import ProductCard from '../../common/ProductCard';
+import GridProducts from '../../components/GridProducts';
+import Layout from '../../components/Layout';
 
 const index = () => {
-  const clothes = [
-    {
-      indice: '1',
-      name: 'Chomba',
-      description: 'Talle Xl, 100% algodón, entrega inmédiata',
-      image: 'https://i.pinimg.com/564x/cc/10/5f/cc105f9492e7222218706c227d3ee96c.jpg',
-      price: 999,
-    },
-    {
-      indice: '2',
-      name: 'Pantalon',
-      description: 'Talle 42, gabardina, entrega en 24hs.',
-      image: 'https://i.pinimg.com/564x/c6/23/09/c6230912bb92db6fbe49f46a4fc00fa2.jpg',
-      price: 666,
-    },
-    {
-      indice: '3',
-      name: 'Campera',
-      description: 'Talle M, cuerina vegetal, no disponible para entrega inmediata ',
-      image: 'https://i.pinimg.com/564x/19/bd/36/19bd369f088ba69c0a97672c39e9a7cd.jpg',
-      price: 333,
-    },
-  ];
-
   return (
-    <>
-      {clothes.map((prenda, i) => (
-        <Box key={i}>
-          <ProductCard product={prenda} />
-        </Box>
-      ))}
-    </>
+    <Layout>
+      <GridProducts />
+    </Layout>
   );
 };
 
