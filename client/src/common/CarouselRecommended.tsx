e232927081fa57106f17a5fd9fd8f8bcd9a5509c
Fran <frangrama510@gmail.com>
2022-10-12 20:41:02 -0300

    fix: recommended carousel

--- client/src/common/CarouselRecommended.tsx
+++ client/src/common/CarouselRecommended.tsx
@@ -1,28 +1,11 @@
 import Carousel from 'react-multi-carousel';
-import { CustomDot } from '../components/CustomCarousel';
-import falseRequest from '../static/clothesNewExample.json';
-import ProductCard from './ProductCard';
 import { Box } from '@mui/material';
 import React, { FC } from 'react';
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 2570 },
-    items: 6,
-  },
-  desktop: {
-    breakpoint: { max: 2570, min: 1629 },
-    items: 5,
-  },
-  tablet: {
-    breakpoint: { max: 1629, min: 464 },
-    items: 4,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
+import ProductCard from './ProductCard';
+import { CustomDot } from '../components/CustomCarousel';
+import falseRequest from '../static/clothesNewExample.json';
+import { responsiveCarousels } from '../utils/responsiveCarousels';
 
 const styles = {
   firstBoxStyle: {
@@ -30,30 +13,32 @@ const styles = {
     position: 'relative',
     display: 'block',
   },
+  childBox: {
+    userSelect: 'none',
+    userDrag: 'none',
+  },
+};
+
+const carouselProps = {
+  additionalTransfrom: 0,
+  responsive: responsiveCarousels.responsive2,
+  pauseOnHover: true,
+  autoPlay: false,
+  infinite: true,
+  showDots: false,
+  draggable: true,
+  arrows: true,
+  minimumTouchDrag: 1,
+  customDot: <CustomDot />,
 };
 
 const CarouselRecommended: FC<{ productId: string }> = ({ productId }) => {
   return (
     <Box component="div" sx={styles.firstBoxStyle}>
-      <Carousel
-        additionalTransfrom={0}
-        responsive={responsive}
-        pauseOnHover={true}
-        autoPlay={false}
-        infinite={true}
-        showDots={false}
-        draggable={true}
-        arrows={true}
-        minimumTouchDrag={1}
-        customDot={<CustomDot />}>
+      <Carousel {...carouselProps}>
         {falseRequest.clothes.map((piece, i) => {
           return (
-            <Box
-              sx={{
-                userSelect: 'none',
-                userDrag: 'none',
-              }}
-              key={i}>
+            <Box sx={styles.childBox} key={i}>
               <ProductCard product={piece} />
             </Box>
           );
