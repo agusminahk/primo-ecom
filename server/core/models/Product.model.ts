7007d3d5f80033c13117975d64cf9d7ccc8ecb46
Francisco Gramajo <101892230+Franchos@users.noreply.github.com>
2022-11-15 09:53:16 -0300

    Revert "Feat : Back product AMD (2) (#12)"
    This reverts commit c538e27b4d19449ddd38a511b6c2fe7681a22a84.

--- server/core/models/Product.model.ts
+++ server/core/models/Product.model.ts
@@ -1,56 +1,24 @@
-import {
-  prop,
-  pre,
-  getModelForClass,
-  modelOptions,
-  Ref,
-  ReturnModelType,
-} from '@typegoose/typegoose';
-
-import { FilterQuery, UpdateQuery } from 'mongoose';
+import { prop, getModelForClass, modelOptions, Ref } from '@typegoose/typegoose';
 import { Category } from './Category.model';
 import { SubCategory } from './SubCategory.model';
 import { User } from './User.model';
 
 type Sizes = 'XL' | 'L' | 'M' | 'S' | 'XS' | number;
 
-class Care {
+class Composition {
   @prop({ type: () => String })
-  wash?: string;
+  cotton?: string;
 
   @prop({ type: () => String })
-  bleach?: string;
+  polyester?: string;
 
   @prop({ type: () => String })
-  iron?: string;
-}
-class Material {
-  @prop({ type: () => Array })
-  exterior: string[];
-
-  @prop({ type: () => Array })
-  sole: string[];
-}
-class Characteristic {
-  @prop({ type: () => String })
-  typeOf: string;
-
-  @prop({ type: () => String })
-  heelHeight: string;
+  elastane?: string;
 }
 
 class Details {
-  @prop({ type: () => Array })
-  material!: Material[];
-
-  @prop({ type: () => Array })
-  characteristic!: Characteristic[];
-
-  @prop({ type: () => Array })
-  composition!: string[];
-
-  @prop({ type: () => Care })
-  care!: Care;
+  @prop({ type: () => Composition, default: undefined })
+  composition!: Composition;
 }
 
 class Image {
@@ -72,83 +40,37 @@ class Review {
   public ranking!: number;
 }
 
-@pre<Product>('save', async function (next) {
-  this.isAvailable = this.quantity > 0;
-  return next();
-})
 @modelOptions({ schemaOptions: { collection: 'product', timestamps: true } })
 export class Product {
-  static async findPopulate(
-    this: ReturnModelType<typeof Product>,
-    options?: FilterQuery<typeof Product>,
-  ) {
-    return this.find({ ...options, isAvailable: true }, { __v: 0 })
-      .populate({
-        path: 'category',
-        select: ' -__v -subCategories',
-      })
-      .populate({
-        path: 'subCategory',
-        select: '_id subCategoryName',
-      });
-  }
-
-  static async updatePopulate(
-    this: ReturnModelType<typeof Product>,
-    id: string,
-    options: UpdateQuery<Partial<Product>>,
-  ) {
-    return this.findOneAndUpdate({ _id: id }, [options], { new: true })
-      .populate({
-        path: 'category',
-        select: ' -__v -subCategories',
-      })
-      .populate({
-        path: 'subCategory',
-        select: '_id subCategoryName',
-      });
-  }
-
-  @prop({ type: () => Boolean })
-  public isAvailable!: boolean;
-
   @prop({ type: () => String, required: true })
   public name!: string;
-
   @prop({ type: () => Number, required: true })
   public price!: number;
-
-  @prop({ type: () => Array, required: true })
+  @prop({ type: () => [String], required: true })
   public colors!: string[];
-
-  @prop({ type: () => String, default: null })
+  @prop({ type: () => String })
   public description?: string;
 
-  @prop({ type: () => Array<Sizes>, required: true })
+  @prop({ type: [String || Number], required: true })
   public sizes!: Sizes[];
 
-  @prop({ type: () => Number, default: 0 })
-  public quantity!: number;
-
-  @prop({ type: () => Array, required: true })
-  public images!: Image[];
+  @prop({ type: () => [Image], default: [] })
+  public images?: Image[];
 
   @prop({ type: () => Number, default: 0 })
   public promotion?: number;
 
-  @prop({ type: () => Details, default: null })
+  @prop({ type: () => Details })
   public details?: Details;
 
   @prop({ type: () => Number, default: 0 })
   public ranking?: number;
 
-  @prop({ type: () => Array })
+  @prop({ type: () => [Review], default: [] })
   public reviews?: Review[];
-
   @prop({ ref: () => Category, required: true })
   public category!: Ref<Category>;
-
   @prop({ ref: () => SubCategory })
-  public subCategory?: Ref<SubCategory>;
+  public subCategory?: Ref<SubCategory>[];
 }
 export default getModelForClass(Product);
